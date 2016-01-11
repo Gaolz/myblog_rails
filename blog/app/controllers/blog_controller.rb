584983e42f24b0441a79716f10d905d88b390564
silentsvv <silentsvv@outlook.com>
2016-01-11 16:19:15 +0800

    prepare_sql2

--- blog/app/controllers/blog_controller.rb
+++ blog/app/controllers/blog_controller.rb
@@ -1,4 +1,5 @@
 class BlogController < ApplicationController
   def welcome
+  	@issues  = [ { title: "Ruby lovers", comments: "saaaaaaaaaaaaaaaaaaaaadsa das dsa das" }, { title: "Rails and Laravel", comments: "5" } ]
   end
 end
